160ee65531c8564a754be935fffa6ae835b0917b
Donalson Javier <donalson1517@gmail.com>
2021-02-04 14:02:38 -0600

    Instalacion de MDI Font
    Se instalo mdf fon para evitar problemas con los iconos e importaciones de estilos de vuetify en el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+
+    <v-app-bar app color="success" dark dense>
+
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -14,30 +12,19 @@
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
       </div>
-
+      
+      <v-toolbar-title>Antojitos Heidy</v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
+
     </v-app-bar>
 
-    <v-main>     
+    <v-main>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
