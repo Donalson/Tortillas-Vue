7702ae464a883f2d63eef2a82f8d69dba45c69ab
Donalson Javier <donalson1517@gmail.com>
2021-02-06 19:05:57 -0600

    Subida de medios
    Se subieron las imagenes que se utilizaran para el menu del proyecto

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
 
     <v-app-bar app color="success" dark dense>
 
+      <v-app-bar-nav-icon @click="mver = !mver"></v-app-bar-nav-icon>
+
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -13,15 +15,79 @@
           width="40"
         />
       </div>
-      
+
       <v-toolbar-title>Antojitos Heidy</v-toolbar-title>
       <v-spacer></v-spacer>
 
+      <v-btn plain> Pedidos <v-icon right>mdi-view-list-outline</v-icon></v-btn>
+      <v-btn plain> Clientes <v-icon right>mdi-account</v-icon></v-btn>
+      <v-btn plain> Tortillas <v-icon right>mdi-alpha-t-circle</v-icon></v-btn>
+      <v-btn plain> Ventas <v-icon right>mdi-receipt</v-icon></v-btn>
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
 
     </v-app-bar>
+
+    <v-navigation-drawer app v-model="mver" temporary color="success">
+      <v-row wrap>
+        <v-col cols="4"></v-col>
+        <v-col class="justify-center" cols="3">
+          <v-img src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" height="45" width="40"/>
+        </v-col>
+        <v-col cols="4"></v-col>
+        <v-col cols="12" class="text-center">
+          <p class="white--text headline">Antojitos Heidy</p>
+        </v-col>
+        <v-col cols="12">
+
+          <v-list>
+            
+            <v-list-item-group class="white--text">
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-view-list-outline</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Pedidos</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item-icon>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-account</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Clientes</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item-icon>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-alpha-t-circle</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Tortillas</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item-icon>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-receipt</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Ventas</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item-icon>
+              </v-list-item>
+
+            </v-list-item-group>
+
+          </v-list>
+
+        </v-col>
+      </v-row>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view/>
@@ -38,7 +104,7 @@ export default {
   },
 
   data: () => ({
-    //
+    mver: false,
   }),
 };
 </script>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="Inicio">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <v-container text-aling-center>
+    <v-row class="justify-center" wrap>
+
+      <v-col
+          v-for="(card, index) in cards" :key="index">
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="black--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.Desc"></v-card-title>
+            </v-img>
+
+          </v-card>
+        </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 
 export default {
   name: 'Inicio',
+
+  data: () => ({
+      cards: [
+        { Desc: 'Pedidos', src: '../assets/Ordenes.png', flex: 6 },
+        { Desc: 'Clientes', src: '../assets/Clientes.png', flex: 6 },
+        { Desc: 'Tortillas', src: '../assets/Tortilla.png', flex: 6 },
+        { Desc: 'Ventas', src: '../assets/Ventas.png', flex: 6 },
+      ],
+    }),
+    
   components: {
   }
 }
