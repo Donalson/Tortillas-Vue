ac145733ab9a3835ee9cca631d6d173a71266459
Donalson Javier <donalson1517@gmail.com>
2021-02-07 20:04:58 -0600

    Version Funcional Local
    Una version funcional del modulo de clientes  pero solo con datos locales

--- src/views/Clientes.vue
+++ src/views/Clientes.vue
@@ -61,15 +61,15 @@
                                     <v-card-title class="headline">Desea macar como inactivo al cliente?</v-card-title>
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
-                                        <v-btn color="warning" text @click="ConfirmarBorrado">Si</v-btn>
-                                        <v-btn color="danger" text @click="CerrarBorrar">Cancelar</v-btn>
+                                        <v-btn color="red" text @click="ConfirmarBorrado">Si</v-btn>
+                                        <v-btn color="green" text @click="CerrarBorrar">Cancelar</v-btn>
                                         <v-spacer></v-spacer>
                                     </v-card-actions>
                                 </v-card>
                             </v-dialog>
                         </v-toolbar>
                     </template>
-                    <template v-slot: item.actions="{ item }">
+                    <template v-slot:item.actions="{ item }">
                         <v-icon small class="mr-2" @click="EditarCliente(item)">mdi-pencil</v-icon>
                         <v-icon small @click="BorrarCliente(item)">mdi-delete</v-icon>
                     </template>
@@ -135,6 +135,7 @@ export default {
                 { text: 'Foto', value: 'Foto' },
                 { text: 'Fecha de Registro', value: 'FC' },
                 { text: 'Fecha de Modificacion', value: 'FE' },
+                { text: 'Acciones', value : 'actions', sortable: false}
             ],
 
             Clientes: [],
@@ -155,13 +156,13 @@ export default {
         EditarCliente (item) {
             this.EditIndex = this.Clientes.indexOf(item)
             this.CEdit = Object.assign({}, item)
-            this.CDD = true
+            this.CD = true
         },
 
         BorrarCliente (item) {
             this.EditIndex = this.Clientes.indexOf(item)
             this.CEdit = Object.assign({}, item)
-            this.CCD = true
+            this.CDD = true
         },
 
         ConfirmarBorrado () {
