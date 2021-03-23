bd8b5a8f4430060768ecd44b414959e3f974a416
Donalson Javier <donalson1517@gmail.com>
2021-03-22 23:47:46 -0600

    Solucion de Notificacion
    La notificacion de ventas pendientes necesitaba una verificacion correcta

--- src/views/Ventas.vue
+++ src/views/Ventas.vue
@@ -351,9 +351,8 @@ export default {
               this.FechasPedidos.push(this.FormatoFecha(Get.data[i].Fecha));
             }
           }
-          this.$alertify.warning("Pedidos Pendientes de registrar en Ventas de Fecha(s) " + Fechas)
-        }else{
-          this.$alertify.success("No hay Pedidos pedientes de registrar")
+          if(Fechas.length>0){this.$alertify.warning("Pedidos Pendientes de registrar en Ventas de Fecha(s) " + Fechas)}
+          else{this.$alertify.success("No hay Pedidos pedientes de registrar")}
         }
       }catch(e){
         console.log
